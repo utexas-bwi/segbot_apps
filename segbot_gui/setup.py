227237dfffacd6dc72ecab35723f61bdbc635d44
Piyush Khandelwal <piyushk@gmail.com>
2015-03-23 19:55:30 -0500

    removed unnecessary script installation which installs in global bin instead of package bin.

--- segbot_gui/setup.py
+++ segbot_gui/setup.py
@@ -6,7 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['segbot_gui'],
     package_dir={'': 'src'},
-    scripts={'scripts/question_dialog_plugin'},
 )
 
 setup(**d)
